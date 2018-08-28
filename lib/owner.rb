318a4fcaa2f0a7cf5cff143156f8b74fa3a866d5
Jenny K <jkam17@gmail.com>
2018-08-28 16:36:24 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -25,7 +25,32 @@ describe Owner do
   let(:fish) { Fish.new("Nemo") }
   let(:cat) { Cat.new("Crookshanks") }
   let(:dog) { Dog.new("Fido") }
+
   context 'instance methods' do
+
+    describe "#species" do
+      it "initializes with a species" do
+        expect(owner.species).to eq("human")
+      end
+
+      it "can't change its species" do
+        expect { owner.species = "hamster" }.to raise_error NoMethodError
+      end
+    end
+
+    describe "#say_species " do
+      it "can say its species" do
+        expect(owner.say_species).to eq("I am a human.")
+      end
+    end
+
+    describe "#name" do
+      it "can have a name" do
+        owner.name = "Katie"
+        expect(owner.name).to eq("Katie")
+      end
+    end
+
     describe "#pets" do
       it "is initialized with a pets attribute as a hash with 3 keys" do
         expect(owner.pets).to eq({:fishes => [], :dogs => [], :cats => []})
